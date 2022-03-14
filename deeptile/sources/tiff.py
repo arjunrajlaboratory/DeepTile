eab0913c8ebf79ca282e2d58a570424c1365f3cd
William Niu <wniu721@gmail.com>
2022-03-14 17:13:28 -0400

    Switch from pure dask to dask-image for TIFF reading.

--- deeptile/sources/tiff.py
+++ deeptile/sources/tiff.py
@@ -1,14 +1,8 @@
-import dask.array as da
-from dask import delayed
-from tifffile import imread
+from dask_image.imread import imread
 
 
 def read(image):
 
-    lazy_imread = delayed(imread)
-    delayed_reader = lazy_imread(image)
-
-    array = imread(image)
-    image = da.from_delayed(delayed_reader, shape=array.shape, dtype=array.dtype)
+    image = imread(image)
 
     return image
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=['dask', 'nd2', 'numpy', 'scikit-image', 'tifffile'],
+    install_requires=['dask', 'dask-image', 'nd2', 'numpy', 'scikit-image', 'tifffile'],
     keywords=["segmentation", "stitching"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
