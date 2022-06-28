85a0a29a8afed9c61aa42d5268ea0d7d02af6ee6
William Niu <wniu721@gmail.com>
2022-06-28 16:58:12 -0400

    Fix data module documentation and bump up to version 1.2.

--- deeptile/data.py
+++ deeptile/data.py
@@ -7,23 +7,26 @@ ALLOWED_STITCHED_TYPES = ('stitched_image', 'stitched_coords')
 class Tiled(np.ndarray):
 
     """ numpy.ndarray subclass for storing tiled data.
-
-    Parameters
-    ----------
-        tiles : numpy.ndarray or Tiled
-            Array of tiles.
-        dt : DeepTile
-            DeepTile instance used to generate this object.
-        otype : str
-            Tiled object type.
-
-    Returns
-    -------
-        tiles : Tiled
-            Array of tiles.
     """
 
     def __new__(cls, tiles, dt, otype):
+
+        """ Create new Tiled object.
+
+        Parameters
+        ----------
+            tiles : numpy.ndarray or Tiled
+                Array of tiles.
+            dt : DeepTile
+                DeepTile instance used to generate this object.
+            otype : str
+                Tiled object type.
+
+        Returns
+        -------
+            tiles : Tiled
+                Array of tiles.
+        """
 
         tiles = np.asarray(tiles).view(cls)
         if otype not in ALLOWED_TILED_TYPES:
@@ -45,23 +48,26 @@ class Tiled(np.ndarray):
 class Stitched(np.ndarray):
 
     """ numpy.ndarray subclass for storing stitched data.
-
-    Parameters
-    ----------
-        stitched : numpy.ndarray or Stitched
-            Stitched array.
-        dt : DeepTile
-            DeepTile instance used to generate this object.
-        otype : str
-            Stitched object type.
-
-    Returns
-    -------
-        stitched : Stitched
-            Stitched array.
     """
 
     def __new__(cls, stitched, dt, otype):
+
+        """ Create new Stitched object.
+
+        Parameters
+        ----------
+            stitched : numpy.ndarray or Stitched
+                Stitched array.
+            dt : DeepTile
+                DeepTile instance used to generate this object.
+            otype : str
+                Stitched object type.
+
+        Returns
+        -------
+            stitched : Stitched
+                Stitched array.
+        """
 
         stitched = np.asarray(stitched).view(cls)
         if otype not in ALLOWED_STITCHED_TYPES:
