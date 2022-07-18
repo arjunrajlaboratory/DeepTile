52b149ecbb9971b34ea71a2a414a6b87b8398250
William Niu <wniu721@gmail.com>
2022-07-18 15:09:08 -0400

    Bump version number to 1.4.0.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.3.0"
+VERSION = "1.4.0"
 DESCRIPTION = "DeepTile"
 LONG_DESCRIPTION = "Large image tiling and stitching algorithm for deep learning libraries."
 
