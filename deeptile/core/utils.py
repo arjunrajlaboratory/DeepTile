ee4aa279e10d154d9956dc0c6a0ef1e5b623dcb7
William Niu <wniu721@gmail.com>
2022-08-16 13:25:45 -0400

    Allow kwargs for array_pad.

--- deeptile/core/utils.py
+++ deeptile/core/utils.py
@@ -100,12 +100,12 @@ def array_split_2d(ary, indices):
     return sub_arys
 
 
-def array_pad(ary, padding, axis=0):
+def array_pad(ary, padding, axis=0, **kwargs):
 
     pad_width = [(0, 0)] * ary.ndim
     pad_width[axis] = (0, padding)
 
-    return np.pad(ary, pad_width)
+    return np.pad(ary, pad_width, **kwargs)
 
 
 def cast_list_to_array(lst):
