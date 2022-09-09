50e18bf20aec30735c9ac1c8cdd171fbc642a828
William Niu <wniu721@gmail.com>
2022-09-09 01:16:39 -0400

    Add example notebook for lifting a custom function.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "2.0.1"
+VERSION = "2.0.2"
 DESCRIPTION = "DeepTile"
 LONG_DESCRIPTION = "Large image tiling and stitching algorithm for deep learning libraries."
 
