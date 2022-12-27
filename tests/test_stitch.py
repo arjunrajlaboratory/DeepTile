0e3f724e938343de5374c1f04a14e7243222b7a8
William Niu <wniu721@gmail.com>
2022-12-27 03:30:35 -0500

    Add test for mask stitching.

--- tests/test_stitch.py
+++ tests/test_stitch.py
@@ -24,3 +24,28 @@ def test_stitch_image():
 
     assert stitched_no_blend.dtype is ground_truth_no_blend.dtype
     assert np.all(stitched_no_blend == ground_truth_no_blend)
+
+
+def test_stitch_masks():
+
+    """ Test that DeepTile can stitch masks. """
+
+    import numpy as np
+    import pickle
+    import tifffile
+    from deeptile import load
+    from deeptile.extensions import stitch
+    from pathlib import Path
+
+    with open(str(Path(__file__).parents[1] / 'data' / 'sample_tif_masks'), 'rb') as f:
+        masks = pickle.load(f)
+
+    ground_truth = \
+        tifffile.imread(str(Path(__file__).parents[1] / 'data' / 'sample_tif_stitched_mask_iou_threshold_10.tif'))
+
+    dt = load(np.zeros((1022, 1024)))
+    tiles = dt.get_tiles(tile_size=(300, 300), overlap=(0.1, 0.1))
+    tiles[:] = masks
+    stitched = stitch.stitch_masks(tiles, iou_threshold=0.1)
+
+    assert np.all(stitched == ground_truth)
