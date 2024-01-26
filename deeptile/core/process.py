7f787791bb54eee87a07ad88caca4d74bdefce48
William Niu <wniu721@gmail.com>
2024-01-26 01:18:03 -0500

    Fix bug in create_batch.

--- deeptile/core/process.py
+++ deeptile/core/process.py
@@ -211,10 +211,10 @@ def create_batch(tiles, pad_final_batch, batch_size, batch_indices):
 
     if isinstance(batch_tiles, Tiled):
         batch_tiles = np.asarray(batch_tiles)
-        if (batch_tiles.dtype is np.dtype('O')) and tiles.metadata['stackable']:
-            batch_tiles = np.stack(batch_tiles)
-            if pad_final_batch and (batch_tiles.shape[0] < batch_size):
-                batch_tiles = utils.array_pad(batch_tiles, batch_size - batch_tiles.shape[0], 0)
+    if isinstance(batch_tiles, np.ndarray) and (batch_tiles.dtype is np.dtype('O')) and tiles.metadata['stackable']:
+        batch_tiles = np.stack(batch_tiles)
+        if pad_final_batch and (batch_tiles.shape[0] < batch_size):
+            batch_tiles = utils.array_pad(batch_tiles, batch_size - batch_tiles.shape[0], 0)
 
     return batch_tiles
 
